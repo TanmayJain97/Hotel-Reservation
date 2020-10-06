bb2e7b0d9f3d9c037508665fc57babd7ae83f466
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-07 00:15:22 +0530

    Add: findCheapestHotel method to find cheapest hotel

--- main/java/com/hotel_reservation/HotelReservation.java
+++ main/java/com/hotel_reservation/HotelReservation.java
@@ -1,6 +1,11 @@
 package com.hotel_reservation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class HotelReservation {
@@ -18,18 +23,10 @@ public class HotelReservation {
 	public void addToList(HotelObject hotel) {
 		hotelList.add(hotel);
 	}
-
-	public boolean addHotel(String hotelName, int rate_regular, String cust_type) {
-		
-		HotelObject hotel = new HotelObject(hotelName,rate_regular,cust_type);
-		addToList(hotel);
-		return true;
-	}
 	
-	//Overload
 	public boolean addHotel(String hotelName, int rate_regular) {
 		
-		HotelObject hotel = new HotelObject(hotelName,rate_regular,"regular");
+		HotelObject hotel = new HotelObject(hotelName,rate_regular);
 		addToList(hotel);
 		return true;
 	}
@@ -38,6 +35,31 @@ public class HotelReservation {
 		for (HotelObject hotel:hotelList) {
 			hotel.display();
 		}
+	}
+	
+	public int daysRented(String start_date, String end_date) {
+		
+		try {
+			Date startDate= new SimpleDateFormat("DD.MM.yyyy").parse(start_date);
+			Date endDate= new SimpleDateFormat("DD.MM.yyyy").parse(end_date);
+			long time_diff = startDate.getTime() - endDate.getTime();
+			return (int) (2+(time_diff / (1000 * 60 * 60 * 24)));
+		}catch(ParseException exception){
+			exception.printStackTrace();
+		}
+		return 0;
+	}
+    
+    public Customer findCheapestHotel(String start_date, String end_date) {
+    	
+    	int daysStayed=daysRented(start_date, end_date);
+    	Optional<HotelObject> cheapestHotelOpt = hotelList.stream().min(Comparator.comparing(
+    			HotelObject::getRate_regular));
+		
+    	HotelObject cheapestHotel = cheapestHotelOpt.get();
+    	int bill=daysStayed*cheapestHotel.getRate_regular();
+    	
+    	return new Customer(cheapestHotel.hotelName, daysStayed, bill);
 	}
 	
 	public static void main( String[] args )
@@ -55,16 +77,38 @@ public class HotelReservation {
         System.out.println("------------------------------------------------------");
         System.out.println();
         
-        System.out.println("Please add hotel.");
-        System.out.println();
-        System.out.print("Enter hotel name: ");
-        String hotelName = sc.next();
-        System.out.print("Enter regular rate of rooms: ");
-        int rate_regular = sc.nextInt();
+        System.out.println("What do you want to do?");
+        System.out.println("1. Add Hotel Entry.");
+        System.out.println("2. Find Cheapest Hotel.");
+        String user_input=sc.next();
         
-        buildObj.addHotel(hotelName, rate_regular);
+        //Initializing main program
+        switch(user_input) {
         
-        //Showing results
-        buildObj.display();
+        case "1": {
+        	System.out.println("Please add hotel.");
+            System.out.println();
+            System.out.print("Enter hotel name: ");
+            String hotelName = sc.next();
+            System.out.print("Enter regular rate of rooms: ");
+            int rate_regular = sc.nextInt();
+            
+            buildObj.addHotel(hotelName, rate_regular);
+            break;
+        }
+        case "2": {
+        	System.out.println("Enter date range to find hotel in format(DD.MM.yyyy)");
+            System.out.println("Enter Check-In date: ");
+            String start_date = sc.next();
+            System.out.println("Enter Check-Out date: ");
+            String end_date = sc.next();
+            Customer cust = buildObj.findCheapestHotel(start_date,end_date);
+            
+            cust.showBill();
+            break;
+        }
+        default:
+        	System.out.println("Unknown input.");
+        }
     }
 }
