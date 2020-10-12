b761042ccddddceea7bd47543dd3a6da8662ae84
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-13 00:02:38 +0530

    Refactor: Test Cases to include check for Reward Rates

--- test/java/com/hotel_reservation/HotelReservationTest.java
+++ test/java/com/hotel_reservation/HotelReservationTest.java
@@ -14,28 +14,28 @@ public class HotelReservationTest {
 	@Before
 	public void init() {
 		hotelReservation = new HotelReservation();
-		hotelReservation.addHotel("Lakewood", 110,90,3);
-		hotelReservation.addHotel("Bridgewood", 160,50,4);
-		hotelReservation.addHotel("Ridgewood", 220,150,5);
+		hotelReservation.addHotel("Lakewood", 110,90,80,80,3);
+		hotelReservation.addHotel("Bridgewood", 160,50,110,50,4);
+		hotelReservation.addHotel("Ridgewood", 220,150,100,40,5);
 	}
 
 	//Testing for creation of 3 hotels
 	@Test
 	public void whenLakewoodAdded_ShouldReturnTrue()
 	{	
-		assertTrue(hotelReservation.addHotel("Lakewood", 110,90,3));
+		assertTrue(hotelReservation.addHotel("Lakewood", 110,90,80,80,3));
 	}
 
 	@Test
 	public void whenBridgewoodAdded_ShouldReturnTrue()
 	{	
-		assertTrue(hotelReservation.addHotel("Bridgewood", 160,50,4));
+		assertTrue(hotelReservation.addHotel("Bridgewood", 160,50,110,50,4));
 	}
 
 	@Test
 	public void whenRidgewoodAdded_ShouldReturnTrue()
 	{	
-		assertTrue(hotelReservation.addHotel("Ridgewood", 220,150,5));
+		assertTrue(hotelReservation.addHotel("Ridgewood", 220,150,100,40,5));
 	}
 
 	//Testing for correct output for staying 1 day
@@ -57,14 +57,14 @@ public class HotelReservationTest {
 	public void whenStayed1DayOnWeekendCheapestHotelShouldBeBridgeWood()
 	{	
 		cust=hotelReservation.findCheapestHotel("12.09.2020", "13.09.2020");
-		//assertEquals("Bridgewood", cust.getHotelName());
+		assertEquals("Bridgewood", cust.getHotelName());
 	}
 
 	@Test
 	public void whenStayed1DayOnWeekendCheapestHotelShouldCost_50()
 	{	
 		cust=hotelReservation.findCheapestHotel("12.09.2020", "13.09.2020");
-		//assertEquals(50, cust.getBill());
+		assertEquals(50, cust.getBill());
 	}
 
 	//Rating Testing With DummyHotel
@@ -72,7 +72,7 @@ public class HotelReservationTest {
 	@Test
 	public void whenStayed1DayCheapestHotelShouldBeDummyHotel()
 	{	
-		hotelReservation.addHotel("DummyHotel", 110,90,5);
+		hotelReservation.addHotel("DummyHotel", 110,90,80,80,5);
 		cust=hotelReservation.findCheapestHotel("12.05.2020", "13.05.2020");
 		assertEquals("DummyHotel", cust.getHotelName());
 	}
@@ -83,5 +83,5 @@ public class HotelReservationTest {
 	{	
 		cust=hotelReservation.bestRatedHotel("12.05.2020", "13.05.2020");
 		assertEquals("Ridgewood", cust.getHotelName());
-	}
+	}	
 }
